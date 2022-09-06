09740c5cde15905e7b625546e08dd5722764e0cc
unknown <patricioponcini10@gmail.com>
2022-09-06 20:25:37 -0300

    Actualizacion

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,71 +1,88 @@
-const {response, request} = require("express");
-const bcryptjs = require("bcryptjs");
-const Usuario = require("../models/usuario");
-const {validationResult} = require("express-validator");
+const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const usuariosGet = (req = request, res = response) => {
-    const {q, nombre = "No name", apikey, page = 1, limit} = req.query;
+
+const Usuario = require('../models/usuario');
+
+
+
+const usuariosGet = async(req = request, res = response) => {
+
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip( Number( desde ) )
+            .limit(Number( limite ))
+    ]);
+
     res.json({
-        msg: "get API - controlador",
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
-    })
+        total,
+        usuarios
+    });
 }
 
-const usuariosPost = async (req, res) => {
+const usuariosPost = async(req, res = response) => {
+    
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
 
-    const {nombre, correo, password, rol} = req.body;
-    const usuario = new Usuario({nombre, correo, password, rol});
-
-    //Encrypt the password
+    // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync(password, salt);
+    usuario.password = bcryptjs.hashSync( password, salt );
 
-    //Save in DB
+    // Guardar en BD
     await usuario.save();
 
     res.json({
         usuario
-    })
+    });
 }
 
-const usuariosPut = async (req, res = response) => {
-    const {id} = req.params;
-    const {_id, password, google, correo, ...resto} =req.body;
+const usuariosPut = async(req, res = response) => {
 
-    if (password){
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
+
+    if ( password ) {
+        // Encriptar la contraseña
         const salt = bcryptjs.genSaltSync();
-        resto.password = bcryptjs.hashSync(password, salt);
+        resto.password = bcryptjs.hashSync( password, salt );
     }
 
-    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+    const usuario = await Usuario.findByIdAndUpdate( id, resto );
 
-    res.json({
-        msg: "put API - controllers",
-        usuario
-    })
+    res.json(usuario);
 }
 
-const usuariosDelete = (req, res) => {
+const usuariosPatch = (req, res = response) => {
     res.json({
-        msg: "delete API - controllers"
-    })
+        msg: 'patch API - usuariosPatch'
+    });
 }
 
-const usuariosPatch = (req, res) => {
-    res.json({
-        msg: "patch API"
-    })
+const usuariosDelete = async(req, res = response) => {
+
+    const { id } = req.params;
+
+    // Fisicamente lo borramos
+    // const usuario = await Usuario.findByIdAndDelete( id );
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
+
+
+    res.json(usuario);
 }
+
+
 
 
 module.exports = {
     usuariosGet,
-    usuariosPut,
     usuariosPost,
+    usuariosPut,
+    usuariosPatch,
     usuariosDelete,
-    usuariosPatch
 }
--- models/server.js
+++ models/server.js
@@ -1,39 +1,56 @@
-const express = require('express')
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('../database/config');
 
 class Server {
-    constructor () {
-        this.app = express();
-        this.port = process.env.PORT;
-        this.usuariosPath = "/api/usuarios";
 
-        //Middlewares
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
         this.middlewares();
 
-        //Rutas de mi aplicación
+        // Rutas de mi aplicación
         this.routes();
     }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
     middlewares() {
-        //CORS
-        this.app.use(cors()); 
 
-        //Lectura y parseo del body
-        this.app.use(express.json());
+        // CORS
+        this.app.use( cors() );
 
-        //Directorio público
-        this.app.use(express.static("public"));  //"use" es la palabra clave para decir que esto es un middleware
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
     }
 
     routes() {
-        this.app.use(this.usuariosPath, require("../routes/usuarios"));
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
     }
 
-    listen(){
-        this.app.listen(this.port, ()=>{
-            console.log("Servidor corriendo en puerto", process.env.PORT)
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
         });
     }
+
 }
+
+
 
 
 module.exports = Server;
