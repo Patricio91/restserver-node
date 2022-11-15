2371bc176040f895ee98a9804db12c56bbaaec9b
unknown <patricioponcini10@gmail.com>
2022-11-14 22:56:07 -0300

    CRUD productos, categ y busq

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -1,5 +1,6 @@
 const {response} = require("express");
 const {Usuario, Categoria, Producto} = require("../models");
+const categoria = require("../models/categoria");
 const {ObjectId} = require("mongoose").Types;
 
 const coleccionesPermitidas = [
@@ -28,6 +29,38 @@ const buscarUsuarios = async(termino = "", res = response) => {
 
 }
 
+const buscarCategorias = async(termino = "", res = response) => {
+    const esMongoId = ObjectId.isValid(termino);
+    if(esMongoId){
+        const categoria = await Categoria.findById(termino);
+        return res.json({
+            results: (categoria) ? [categoria] : []
+        })
+    }
+    const regex = new RegExp(termino, "i");
+    const categorias = await Categoria.find({nombre: regex, estado: true});
+    res.json({
+        results: categorias
+    })
+
+}
+
+const buscarProductos = async(termino = "", res = response) => {
+    const esMongoId = ObjectId.isValid(termino);
+    if(esMongoId){
+        const producto = await Producto.findById(termino).populate("categoria", "nombre");
+        return res.json({
+            results: (producto) ? [producto] : []
+        })
+    }
+    const regex = new RegExp(termino, "i");
+    const productos = await Producto.find({nombre: regex, estado: true}).populate("categoria", "nombre");;
+    res.json({
+        results: productos
+    })
+
+}
+
 const buscar = (req, res = response) => {
     const {coleccion, termino} = req.params;
     if(!coleccionesPermitidas.includes(coleccion)){
@@ -39,11 +72,11 @@ const buscar = (req, res = response) => {
         case "usuarios":
             buscarUsuarios(termino, res);
         break;
-
         case "categorias":
+            buscarCategorias(termino, res);
         break;
-
         case "productos":
+            buscarProductos(termino, res);
         break;
 
         default:
